7328b856c20d97d4f1350ce47806ff7ad407bef4
Richard <jsksosdgkd29y@gmail.com>
2020-04-18 14:48:01 -0400

    works on my machine, need some sort of database

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
-from flask import Flask
+from flask import Flask, request
+from reverse import reverse_gif
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello World!"
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    errors = ''
+    if request.method == "POST":
+        gif_url = None
+        try:
+            gif_url = request.form['url']
+        except:
+            errors += 'Something went wrong.\n'        
+        return '''
+                <html>
+                    <body>
+                        <p>The result is {result}</p>
+                        <p><a href="/">Click here to reverse another gif.</a>
+                    </body>
+                </html>
+            '''.format(result=reverse_gif(gif_url))
+
+    return '''
+        <html>
+            <body>
+                {errors}
+                <p>Enter the URL to GIF:</p>
+                <form method="post" action=".">
+                    <p><input name="url" /></p>
+                    <p><input type="submit" value="Reverse!" /></p>
+                </form>
+            </body>
+        </html>
+    '''.format(errors=errors)
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
--- reverse.py
+++ reverse.py
@@ -2,15 +2,16 @@ from PIL import Image, ImageSequence
 import requests
 import os
 
-url_to_gif = input("Enter url to gif: ")
-response = requests.get(url_to_gif, stream=True)
-img = Image.open(response.raw)
-frame_count = img.n_frames
-frames = []
+def reverse_gif(url_to_gif):
+    response = requests.get(url_to_gif, stream=True)
+    img = Image.open(response.raw)
+    frame_count = img.n_frames
+    frames = []
+    for frame in ImageSequence.Iterator(img):
+        frames.append(frame.copy())
+    frames.reverse()
+    frames[0].save('reversed.gif', format='GIF', append_images=frames[1:], save_all=True, loop=0)
+    return os.path.join(os.getcwd(), 'reversed.gif')
 
-for frame in ImageSequence.Iterator(img):
-    frames.append(frame.copy())
-frames.reverse()
-
-frames[0].save('reversed.gif', format='GIF', append_images=frames[1:], save_all=True, loop=0)
-print(os.path.join(os.getcwd(), 'reversed.gif'))
+if __name__ == "__main__":
+    reverse_gif(input("Enter url to gif: "))
