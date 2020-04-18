b8f8c17f81af746cccfbd1cec4aae14cddab2b94
Richard <jsksosdgkd29y@gmail.com>
2020-04-18 16:25:28 -0400

    has a caching issue, needs to hard refresh (shift + F5) for new gif to load

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request
 from reverse import reverse_gif
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+# max size 10MB
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -11,24 +13,36 @@ def home():
         try:
             gif_url = request.form['url']
         except:
-            errors += 'Something went wrong.\n'        
+            errors += 'Something went wrong.\n'
+        reverse_gif(gif_url)
         return '''
                 <html>
+                <head>
+                    <title>Result</title>
+                    <link rel='icon' href='static/favicon.ico' type='image/x-icon'/>
+                </head>
                     <body>
-                        <p>The result is {result}</p>
+                        <p><img src="static/reversed.gif" alt="reversed-gif"></p>
                         <p><a href="/">Click here to reverse another gif.</a>
                     </body>
                 </html>
-            '''.format(result=reverse_gif(gif_url))
+            '''
 
     return '''
         <html>
+            <head>
+                <title>GIF reverser</title>
+                <link rel='icon' href='static/favicon.ico' type='image/x-icon'/>
+            </head>
             <body>
                 {errors}
+                <h1>Reverse your Gifs!</h1>
                 <p>Enter the URL to GIF:</p>
                 <form method="post" action=".">
                     <p><input name="url" /></p>
                     <p><input type="submit" value="Reverse!" /></p>
+                    <br>
+                    <p>Made by Richard Li @ Northeastern University</p>
                 </form>
             </body>
         </html>
--- reverse.py
+++ reverse.py
@@ -3,6 +3,10 @@ import requests
 import os
 
 def reverse_gif(url_to_gif):
+    response = requests.head(url_to_gif, stream=True)
+    if int(response.headers['Content-Length']) > 10000000:
+        print('too big')
+        return ''
     response = requests.get(url_to_gif, stream=True)
     img = Image.open(response.raw)
     frame_count = img.n_frames
@@ -10,8 +14,8 @@ def reverse_gif(url_to_gif):
     for frame in ImageSequence.Iterator(img):
         frames.append(frame.copy())
     frames.reverse()
-    frames[0].save('reversed.gif', format='GIF', append_images=frames[1:], save_all=True, loop=0)
-    return os.path.join(os.getcwd(), 'reversed.gif')
+    frames[0].save(os.path.join('static', 'reversed.gif'), format='GIF', append_images=frames[1:], save_all=True, loop=0)
+    return os.path.join(os.getcwd(), 'static', 'reversed.gif')
 
 if __name__ == "__main__":
     reverse_gif(input("Enter url to gif: "))
